398d5707fad8c4936c3219e2a6ff032ee4d0a61d
Marcelo Dauane <marcelo@bnry.digital>
2020-08-31 16:18:13 +0200

    Initial commit

---
+++ pages/covid-solidarity/donate.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react'
+
+export default function Donate() {
+  useEffect(() => {
+    document.location.href =
+      'https://tickets.computicket.com/event/shoprite_group_virtual_vouchers/7145292/7145306/107235'
+  })
+
+  return null
+}
---
+++ pages/covid-solidarity/project.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react'
+
+export default function Project() {
+  useEffect(() => {
+    document.location.href = 'https://trello.com/b/Ah3VPMV0/nipfuni-solidarity-campaign'
+  })
+
+  return null
+}
